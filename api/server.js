b23b53fb9f34870146d10c58dcdb6116322f92ff
Md Sultan Mahmud Munna <munna.syc@gmail.com>
2025-03-31 16:31:02 +0600

    Updated vercel

--- api/server.js
+++ api/server.js
@@ -9,6 +9,7 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 // Load recipes from recipe.json at startup
 let recipes = [];
