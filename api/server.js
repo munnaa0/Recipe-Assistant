c445ae412c42b1fd9e0207066867900ca09262af
Md Sultan Mahmud Munna <munna.syc@gmail.com>
2025-03-31 18:18:48 +0600

    Update server.js

--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 4587;
 
 // Middleware
 app.use(cors());
